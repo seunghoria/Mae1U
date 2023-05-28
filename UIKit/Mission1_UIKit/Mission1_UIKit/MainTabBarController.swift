be257951d2d23b2c2b48e888500d4a67097ec9d2
seunghoria <seungeun991114@gmail.com>
2023-05-28 20:56:04 +0900

    [FEAT] 탭바 컨트롤러 Drop Shadow 추가(#3)

--- UIKit/Mission1_UIKit/Mission1_UIKit/MainTabBarController.swift
+++ UIKit/Mission1_UIKit/Mission1_UIKit/MainTabBarController.swift
@@ -8,13 +8,15 @@
 import UIKit
 
 class MainTabBarController: UITabBarController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-    
+        setupStyle()
     }
 
-
+    func setupStyle() {
+            UITabBar.clearShadow()
+            tabBar.layer.applyShadow(color: .black, alpha: 0.1, x: 0, y: -1, blur: 4)
+    }
 
 }
 
