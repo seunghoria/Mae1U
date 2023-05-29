96384344be353b649625367cc81e59872b617b24
seunghoria <seungeun991114@gmail.com>
2023-05-29 14:49:18 +0900

    [FEAT] 본인인증서 카드 Rectangle 모서리 둥글게 구현(#5)

--- UIKit/Mission1_UIKit/Mission1_UIKit/ViewController.swift
+++ UIKit/Mission1_UIKit/Mission1_UIKit/ViewController.swift
@@ -11,14 +11,17 @@ class ViewController: UIViewController {
     
     
     @IBOutlet weak var navigationLabel: UILabel!
+    @IBOutlet weak var navigationButton: UIButton!
+    
     
     @IBOutlet weak var cardMain: UIView!
-    
     @IBOutlet weak var cardTitle: UILabel!
+    @IBOutlet weak var certificationButton: UIButton!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        cardMain.layer.cornerRadius = 16
     }
     
     
@@ -26,10 +29,12 @@ class ViewController: UIViewController {
     @IBAction func certificationButtonPressed(_ sender: UIButton) {
         
         navigationLabel.text = "코로나19 예방접종 내역"
+        navigationButton.backgroundColor = #colorLiteral(red: 0.1215686275, green: 0.3529411765, blue: 1, alpha: 1)
         
         cardMain.backgroundColor = #colorLiteral(red: 0.09803921569, green: 0.2862745098, blue: 0.7019607843, alpha: 1)
-        
         cardTitle.text = "코로나19 예방접종 증명서"
+        
+        certificationButton.layer.isHidden = true
     
         
     }
