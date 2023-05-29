e352b0ea7d2aa8fc37891b7c0fa2e06ce7ce948e
seunghoria <seungeun991114@gmail.com>
2023-05-29 13:54:15 +0900

    [FEAT] 버튼 터치 시 카드 색상 및 타이틀 변경(#6)

--- UIKit/Mission1_UIKit/Mission1_UIKit/ViewController.swift
+++ UIKit/Mission1_UIKit/Mission1_UIKit/ViewController.swift
@@ -12,6 +12,9 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var navigationLabel: UILabel!
     
+    @IBOutlet weak var cardMain: UIView!
+    
+    @IBOutlet weak var cardTitle: UILabel!
     
     
     override func viewDidLoad() {
@@ -23,6 +26,11 @@ class ViewController: UIViewController {
     @IBAction func certificationButtonPressed(_ sender: UIButton) {
         
         navigationLabel.text = "코로나19 예방접종 내역"
+        
+        cardMain.backgroundColor = #colorLiteral(red: 0.09803921569, green: 0.2862745098, blue: 0.7019607843, alpha: 1)
+        
+        cardTitle.text = "코로나19 예방접종 증명서"
+    
         
     }
     
