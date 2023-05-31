74dec006d71272de392f1a2253f3ec5fff5ae282
seunghoria <seungeun991114@gmail.com>
2023-06-01 00:34:29 +0900

    [FEAT] 버튼 터치 시 카드 타이틀 및 정보 텍스트 변경(#6)

--- UIKit/Mission1_UIKit/Mission1_UIKit/ViewController.swift
+++ UIKit/Mission1_UIKit/Mission1_UIKit/ViewController.swift
@@ -19,6 +19,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var cardWhiteBox: UIView!
     @IBOutlet weak var certificationButton: UIButton!
     
+    @IBOutlet weak var certificatedTitle: UILabel!
+    @IBOutlet weak var certificatedInfo: UILabel!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,12 +42,16 @@ class ViewController: UIViewController {
     @IBAction func certificationButtonPressed(_ sender: UIButton) {
         
         navigationLabel.text = "코로나19 예방접종 내역"
+        navigationButton.layer.cornerRadius = 13.5
         navigationButton.backgroundColor = #colorLiteral(red: 0.1215686275, green: 0.3529411765, blue: 1, alpha: 1)
         
         cardMain.backgroundColor = #colorLiteral(red: 0.09803921569, green: 0.2862745098, blue: 0.7019607843, alpha: 1)
-        cardTitle.text = "코로나19 예방접종 증명서"
         
+        cardTitle.layer.isHidden = true
         certificationButton.layer.isHidden = true
+        
+        certificatedTitle.layer.isHidden = false
+        certificatedInfo.layer.isHidden = false
     
         
     }
